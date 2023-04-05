63261f88ff872a6816a2c9775fdd389d97d6e2ba
Guilherme Rosario <guilhermerosarioa@gmail.com>
2023-04-05 01:49:05 -0300

    finalizacao

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,5 @@
-/* Capturar os inputs */
-const texto = document.getElementById('resultadoTexto');
 const form = document.getElementById('formImc');
-/* Fazer a página enviar o form e não reiniciar a página */
+const texto = document.getElementById('resultadoTexto');
 
 function submitForm(event) {
   event.preventDefault();
@@ -11,11 +9,23 @@ function calcular() {
   const botao = document.getElementById('botaoCalcular');
   const valorPeso = document.getElementById('inputPeso').value;
   const valorAltura = document.getElementById('inputAltura').value;
+  const texto = document.getElementById('resultadoTexto');
+  let resultado;
 
-  let resultado = valorPeso / (valorAltura * valorAltura);
+  const IMC = valorPeso / (valorAltura * valorAltura);
+  if (IMC < 18.5) {
+    mensagem = 'abaixo do peso';
+  } else if (IMC >= 18.5 && IMC <= 29.9) {
+    mensagem = 'com sobrepeso';
+  } else if (IMC >= 30 && IMC <= 34.9) {
+    mensagem = 'com obesidade grau 1';
+  } else if (IMC >= 35 && IMC <= 39.9) {
+    mensagem = 'com obesidade grau 2';
+  } else if (IMC >= 40) {
+    mensagem = 'com obesidade grau 3';
+  }
 
-  return resultado;
+  texto.innerHTML = `<p> Seu IMC é ${IMC} e você está ${mensagem} </p>`;
 }
-
 botaoCalcular.addEventListener('click', calcular);
 form.addEventListener('submit', submitForm);
