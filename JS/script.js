90eed21e618978342ac846fb506cb5bf77d60367
Guilherme Rosario <guilhermerosarioa@gmail.com>
2023-04-05 00:49:12 -0300

    Adicionado funcao ao botao calcular

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,21 @@
 /* Capturar os inputs */
-
-const inputPeso = document.getElementById('inputPeso');
-const inputAltura = document.getElementById('inputAltura');
-const botaoCalcular = document.getElementById('buttonCalcular');
-const texto = document.getElementById('resultado');
+const texto = document.getElementById('resultadoTexto');
 const form = document.getElementById('formImc');
 /* Fazer a página enviar o form e não reiniciar a página */
+
 function submitForm(event) {
   event.preventDefault();
 }
 
+function calcular() {
+  const botao = document.getElementById('botaoCalcular');
+  const valorPeso = document.getElementById('inputPeso').value;
+  const valorAltura = document.getElementById('inputAltura').value;
+
+  let resultado = valorPeso / (valorAltura * valorAltura);
+
+  return resultado;
+}
+
+botaoCalcular.addEventListener('click', calcular);
 form.addEventListener('submit', submitForm);
